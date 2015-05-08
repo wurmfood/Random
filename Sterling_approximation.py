28d0bcaf229f188b9ef670fb3623256333d905ec
wurmfood <wurmfood@gmail.com>
2015-05-08 15:13:23 -0700

    Fix mistake in error calculation. Added Sterling's second method of calculating n! Moved various bits into lists to clean things up.

--- Sterling_approximation.py
+++ Sterling_approximation.py
@@ -14,10 +14,33 @@ import sys
 import getopt
 
 
+###############################
+#
+# Approximation method:
+# ln(n!) = n*ln(n) -n
+#
+###############################
 def sterling_approximation(n):
     n_fact = math.log(math.factorial(n))
     st_approx = n*math.log(n) - n
-    err = math.fabs((n_fact - st_approx)/st_approx)
+    err = None
+    try:
+        err = math.fabs((n_fact - st_approx)/n_fact)
+    except ZeroDivisionError:
+        err = 0
+    return n_fact, st_approx, err
+
+
+#################################
+#
+# Approximation method:
+# n! = n^n * e^(-n) * sqrt(2 * Pi * n)
+#
+#################################
+def sterling_approximation2(n):
+    n_fact = math.factorial(n)
+    st_approx = math.pow(n, n)*math.exp(-n)*math.sqrt(2*math.pi*n)
+    err = math.fabs((n_fact - st_approx)/n_fact)
     return n_fact, st_approx, err
 
 
@@ -29,10 +52,16 @@ def main(argv):
         max_num = int(a)
 
     try:
-        print('{0:^4} : {1:^10} \t {2:^10} \t {3:^10}'.format('n', 'ln(n!)', 'Approx', 'Err%'))
+        headers = ['n', 'ln(n!)', 'Approx', 'Err%', 'n!', 'Approx', 'Err%']
+        print('{0:^4} : {1:^10} \t {2:^10} \t {3:^10} \t {4:^10} \t {5:^10} \t {6:^10}'.format(*headers))
         for i in range(1, max_num+1):
-            fact, approx, error = sterling_approximation(i)
-            print('{0: <4} : {1: >10.5f} \t {2: >10.5f} \t {3: >8.3%}'.format(i, fact, approx, error))
+            results = []
+            results[0:2] = sterling_approximation(i)
+            results[3:5] = sterling_approximation2(i)
+            print('{0: <4} : {1: >10.5f} '
+                  '\t {2: >10.5f} \t {3: >8.3%} '
+                  '\t {4: >10.5e} \t {5: >10.5e} '
+                  '\t {6: >8.3%}'.format(i, *results))
     finally:
         pass
 
